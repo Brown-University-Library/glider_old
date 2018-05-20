f46ef1bccf6452158b0b4713d549f2677427a689
jsalisbury <joel.salisbury@gmail.com>
2018-05-20 12:55:05 -0400

    more

--- js/FlightPlan.js
+++ js/FlightPlan.js
@@ -1,20 +1,59 @@
 class FlightPlan {
+  
   constructor() {
-    this.placesPath = "places.xml";
-    this.partsPath = "parts.xml";
-    this.phases;
+    console.log("constructing flight plan...");
+    this.phaseList;
+    this.currentPhaseIndex = 0;
+    this.currentPhase;
     this.buildPhases();
-  }
-
-  ingestPlaces() {
-
-  }
-
-  ingestParts() {
-  	// build parts from the path identified in the 
+    this.renderCurrentPhase();
+    this.setupKeyPressListeners();
   }
 
   buildPhases() {
-  	var phases = document.querySelectorAll("phase");
+    console.log("building phases...");
+  	this.phaseList = document.querySelectorAll("phase");
+  }
+
+  setupKeyPressListeners () {
+    var hoist = this; // not really but
+    document.onkeydown = function(e) {
+
+        e = e || window.event;
+        if (e.keyCode == '37') {
+           hoist.nextPhase();
+        }
+        else if (e.keyCode == '39') {
+           hoist.prevPhase();
+        }
+    }
+  }
+
+  nextPhase() {
+    this.currentPhaseIndex--;
+    this.renderCurrentPhase();
+  }
+
+  prevPhase() {
+    this.currentPhaseIndex++;
+    this.renderCurrentPhase();
+  }
+
+  renderCurrentPhase(){
+    //console.log("my phaseIndex is" . this.currentPhaseIndex);
+    this.currentPhase = this.phaseList[this.currentPhaseIndex];
+
+    console.log("...starting phase # " + this.currentPhaseIndex);
+
+    let displayList = this.currentPhase.querySelectorAll("display");
+    //console.log(displayList);
+
+    for (var i=0; i< displayList.length; i++) {
+      var display = displayList[i];
+      var part = display.getAttribute("part");
+      var place = display.getAttribute("place");
+      //console.log(display.getAttribute("part"));
+      console.log("display part # " + part + " on place # " + place);
+    }
   }
 }
